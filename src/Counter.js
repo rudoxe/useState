6d39eeeefbf6114f9a1d48eba4a8f5e78916f85b
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-16 11:31:14 +0300

    izveidots state projekts

---
+++ src/Counter.js
@@ -0,0 +1,17 @@
+import React, { useState } from 'react';
+
+function Counter() {
+    const [count, setCount] = useState(0);
+    return (
+        <div>
+            <button onClick={() => setCount(count + 1)}>+1</button>
+            <h1>{count}</h1>
+            <button onClick={() => setCount(count - 1)}>-1</button>
+        </div>
+    );
+}
+
+
+
+
+export default Counter;

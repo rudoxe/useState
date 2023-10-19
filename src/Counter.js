909c03bdc77f251e2c993f56f359afc66f8fc7a3
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-19 11:35:53 +0300

    addo reizin

--- src/Counter.js
+++ src/Counter.js
@@ -1,17 +1,26 @@
-import React, { useState } from 'react';
+import React, { useState } from "react";
 
 function Counter() {
-    const [count, setCount] = useState(0);
-    return (
-        <div>
-            <button onClick={() => setCount(count + 1)}>+1</button>
-            <h1>{count}</h1>
-            <button onClick={() => setCount(count - 1)}>-1</button>
-        </div>
-    );
+  const [count, setCount] = useState(0);
+  const[multiply, setMultiply] = useState(2);
+
+
+  function multipy(){
+    setCount(count * multiply);
+  }
+  function handleChange(event){
+    setMultiply(event.target.value);
+  }
+  return (
+    <div>
+ 
+      <button onClick={() => setCount(count + 1)}>+1</button>
+      <button onClick={() => setCount(count - 1)}>-1</button>
+      <input type="number" value={multiply} onChange={handleChange} />
+      <button onClick={() => multipy()}>reizinat</button>
+      <h1>{count}</h1>
+    </div>
+  );
 }
-
-
-
 
 export default Counter;
